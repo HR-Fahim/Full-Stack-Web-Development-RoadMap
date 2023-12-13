e09cb3251abc8b1dfa5e4b9bb461cf76fa57fea6
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-14 03:03:18 +0600

    Initialize Files

--- Part 1 -  Introduction to React/Component State, Event Handlers/part1/src/App.jsx
+++ Part 1 -  Introduction to React/Component State, Event Handlers/part1/src/App.jsx
@@ -6,14 +6,14 @@ import './App.css'
 
 // import React from 'react';
 
-const Hello = (props) => {
-
-  const { name, age } = props
+const Hello = ({ name, age }) => {
   const bornYear = () => new Date().getFullYear() - age
 
   return (
     <div>
-      <p>Hello {name}, you are {age} years old</p>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
   )
