7cc58277f43a835cc20362f4cbd52cc805229689
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-13 22:55:32 +0600

    Initialize Files

--- Part 1 -  Introduction to React/Introduction to React/part1/src/App.jsx
+++ Part 1 -  Introduction to React/Introduction to React/part1/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
@@ -5,20 +6,22 @@ import './App.css'
 
 // import React from 'react';
 
-const Hello = () => {
-  return (
-    <div>
-      <p>Hello world</p>
-    </div>
-  )
-}
-
 const App = () => {
   return (
     <div>
       <h1>Greetings</h1>
 
-      <Hello />
+      <Hello name='George' />
+      <Hello name='Daisy' />
+    </div>
+  )
+}
+
+const Hello = (props) => {
+  return (
+    <div>
+
+      <p>Hello {props.name}</p>
     </div>
   )
 }
