f760192f585f2b38f32a7d968919fc8c9d3e1f35
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-13 22:44:22 +0600

    Initialize Files

--- Part 1 -  Introduction to React/Introduction to React/part1/src/App.jsx
+++ Part 1 -  Introduction to React/Introduction to React/part1/src/App.jsx
@@ -3,21 +3,23 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 
-import React from 'react';
+// import React from 'react';
+
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
 
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'p', null, 'Hello world, it is ', now.toString()
-    ),
-    React.createElement(
-      'p', null, a, ' plus ', b, ' is ', a + b
-    )
+  return (
+    <div>
+      <h1>Greetings</h1>
+
+      <Hello />
+    </div>
   )
 }
 
