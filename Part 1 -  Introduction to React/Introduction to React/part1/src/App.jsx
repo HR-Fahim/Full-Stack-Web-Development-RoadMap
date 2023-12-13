02df4dba30de917ca1c3b34cb1e2793cf77da7c6
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-13 23:12:28 +0600

    Initialize Files

--- Part 1 -  Introduction to React/Introduction to React/part1/src/App.jsx
+++ Part 1 -  Introduction to React/Introduction to React/part1/src/App.jsx
@@ -7,15 +7,11 @@ import './App.css'
 // import React from 'react';
 
 const App = () => {
-  const friends = [
-    { name: 'Peter', age: 4 },
-    { name: 'Maya', age: 10 },
-  ]
+  const friends = [ 'Peter', 'Maya']
 
   return (
     <div>
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
+      <p>{friends}</p>
     </div>
   )
 }
