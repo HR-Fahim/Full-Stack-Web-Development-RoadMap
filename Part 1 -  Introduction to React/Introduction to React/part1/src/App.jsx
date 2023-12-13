9f5ebaaf2dc0eba6e1d439ab9154781d93eceee6
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-13 22:58:43 +0600

    Initialize Files

--- Part 1 -  Introduction to React/Introduction to React/part1/src/App.jsx
+++ Part 1 -  Introduction to React/Introduction to React/part1/src/App.jsx
@@ -6,22 +6,30 @@ import './App.css'
 
 // import React from 'react';
 
-const App = () => {
+const Hello = (props) => {
+
+  console.log(props)
   return (
     <div>
-      <h1>Greetings</h1>
+      <p>
 
-      <Hello name='George' />
-      <Hello name='Daisy' />
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
-const Hello = (props) => {
+const App = () => {
+
+  const name = 'Peter'
+  const age = 10
+
   return (
     <div>
+      <h1>Greetings</h1>
 
-      <p>Hello {props.name}</p>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
