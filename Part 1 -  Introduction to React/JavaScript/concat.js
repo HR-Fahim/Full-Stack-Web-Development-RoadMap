8bd33e0ec8a09a4eb9ca7d405a8cd444b5d6cce9
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-14 01:52:52 +0600

    Initialize Files

--- Part 1 -  Introduction to React/JavaScript/concat.js
+++ Part 1 -  Introduction to React/JavaScript/concat.js
@@ -5,7 +5,16 @@ const t2 = t.concat(5)  // creates new array
 console.log(t)  // [1, -1, 3] is printed
 console.log(t2) // [1, -1, 3, 5] is printed
 
+
 const t3 = [1, 2, 3]
 
 const m1 = t3.map(value => value * 2)
 console.log(m1)   // [2, 4, 6] is printed
+
+
+const t4 = [1, 2, 3, 4, 5]
+
+const [first, second, ...rest] = t4
+
+console.log(first, second)  // 1, 2 is printed
+console.log(rest)          // [3, 4, 5] is printed
