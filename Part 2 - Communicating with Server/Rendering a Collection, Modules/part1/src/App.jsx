d680686fd2429f09173953f1db19aff16a2e1085
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-15 03:14:56 +0600

    Implement bug fix for issue

---
+++ Part 2 - Communicating with Server/Rendering a Collection, Modules/part1/src/App.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+// import React from 'react';
+
+const App = (props) => {
+  const { notes } = props
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
