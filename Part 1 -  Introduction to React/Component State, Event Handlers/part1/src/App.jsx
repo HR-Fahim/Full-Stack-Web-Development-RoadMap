5e80bd2142e5bfa15dcf825344616b6a72b00dec
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-14 21:54:01 +0600

    Initialize Files

--- Part 1 -  Introduction to React/Component State, Event Handlers/part1/src/App.jsx
+++ Part 1 -  Introduction to React/Component State, Event Handlers/part1/src/App.jsx
@@ -9,14 +9,15 @@ import './App.css'
 import { useState } from 'react'
 
 const App = () => {
-
   const [ counter, setCounter ] = useState(0)
-
 
   setTimeout(
     () => setCounter(counter + 1),
     1000
   )
+
+
+  console.log('rendering...', counter)
 
   return (
     <div>{counter}</div>
