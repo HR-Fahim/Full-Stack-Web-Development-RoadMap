12cfaa9e2a9ffa980628b37d32a05546e67e7677
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-15 03:29:32 +0600

    Refactor App component to use destructuring

--- Part 2 - Communicating with Server/Rendering a Collection, Modules/part1/src/App.jsx
+++ Part 2 - Communicating with Server/Rendering a Collection, Modules/part1/src/App.jsx
@@ -7,15 +7,12 @@ import './App.css'
 
 // import React from 'react';
 
-const App = (props) => {
-  const { notes } = props
-
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map(note => 
-
           <li key={note.id}>
             {note.content}
           </li>
