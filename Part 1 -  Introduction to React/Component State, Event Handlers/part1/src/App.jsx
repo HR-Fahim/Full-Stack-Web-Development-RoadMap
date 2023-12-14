b9dff7488201c3f5aae7920c15de612427061d6e
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-14 22:29:12 +0600

    Initialize Files

--- Part 1 -  Introduction to React/Component State, Event Handlers/part1/src/App.jsx
+++ Part 1 -  Introduction to React/Component State, Event Handlers/part1/src/App.jsx
@@ -8,23 +8,26 @@ import './App.css'
 
 import { useState } from 'react'
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-
   const increaseByOne = () => setCounter(counter + 1)
-  
   const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <div>{counter}</div>
 
+      <Display counter={counter}/>
       <button onClick={increaseByOne}>
         plus
       </button>
-
-      <button onClick={setToZero}>
+      <button onClick={setToZero}> 
         zero
       </button>
     </div>
