292bc7f60fbcb8af5f5cb1cfcd81e44327db1bf7
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-15 03:28:25 +0600

    Refactor note rendering in App component

--- Part 2 - Communicating with Server/Rendering a Collection, Modules/part1/src/App.jsx
+++ Part 2 - Communicating with Server/Rendering a Collection, Modules/part1/src/App.jsx
@@ -13,9 +13,13 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
-
       <ul>
-        {notes.map(note => <li>{note.content}</li>)}
+        {notes.map(note => 
+
+          <li key={note.id}>
+            {note.content}
+          </li>
+        )}
       </ul>
     </div>
   )
