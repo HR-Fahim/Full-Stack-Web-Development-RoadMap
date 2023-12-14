425f8c526207cd60f8877c1b354047383cb36e83
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-15 01:35:27 +0600

    Initialize Files

--- Part 1 -  Introduction to React/A More Complex State, Debugging React Apps/part1/src/App.jsx
+++ Part 1 -  Introduction to React/A More Complex State, Debugging React Apps/part1/src/App.jsx
@@ -8,53 +8,52 @@ import './App.css'
 
 import { useState } from 'react'
 
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const [total, setTotal] = useState(0)
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-
-    setTotal(left + right)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-
-    setTotal(left + right)
-  }
-
-  const History = (props) => {
-    if (props.allClicks.length === 0) {
-      return (
-        <div>
-          the app is used by pressing the buttons
-        </div>
-      )
-    }
+const History = (props) => {
+  if (props.allClicks.length === 0) {
     return (
       <div>
-        button press history: {props.allClicks.join(' ')}
+        the app is used by pressing the buttons
       </div>
     )
   }
 
   return (
     <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
       {right}
-      <p>{allClicks.join(' ')}</p>
-
       <History allClicks={allClicks} />
-
-      <p>total {total}</p>
     </div>
   )
 }
