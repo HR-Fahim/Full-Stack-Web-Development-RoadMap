e33d85c91c3b060bd33bfa11b73ee57d8de2153f
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-15 21:21:55 +0600

    Add useState hook and form to add notes

--- Part 2 - Communicating with Server/Forms/part1/src/App.jsx
+++ Part 2 - Communicating with Server/Forms/part1/src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
@@ -13,16 +13,28 @@ const Note = ({ note }) => {
   )
 }
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes] = useState(props.notes)
+
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-
         {notes.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
+
+      <form onSubmit={addNote}>
+        <input />
+        <button type="submit">save</button>
+      </form>   
     </div>
   )
 }
