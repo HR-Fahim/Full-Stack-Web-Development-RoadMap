4369eb569decba6c89dbd7d9856ab448444e7b33
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-16 00:54:06 +0600

    Update code formatting and fix minor bugs

---
+++ Part 2 - Communicating with Server/Getting Data from Server/part1/src/App.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+// import React from 'react';
+
+const Note = ({ note }) => {
+  return (
+    <li>{note.content}</li>
+  )
+}
+
+const App = (props) => {
+  const [notes] = useState(props.notes)
+  const [showAll, setShowAll] = useState(true)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  ) 
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>   
+    </div>
+  )
+}
+
+export default App
