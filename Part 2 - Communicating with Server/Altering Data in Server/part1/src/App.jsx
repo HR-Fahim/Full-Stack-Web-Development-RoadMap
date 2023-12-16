265bd2db98f9cf2b7074e55d4e9014464795bb15
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-16 22:53:31 +0600

    Add Note component to App.jsx

---
+++ Part 2 - Communicating with Server/Altering Data in Server/part1/src/App.jsx
@@ -0,0 +1,93 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+// import React from 'react';
+
+import axios from 'axios'
+
+// const Note = ({ note }) => {
+//   return (
+//     <li>{note.content}</li>
+//   )
+// }
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  // const [notes] = useState(props.notes)
+  const [showAll, setShowAll] = useState(true)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  ) 
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+
+  const Note = ({ note, toggleImportance }) => {
+    const label = note.important
+      ? 'make not important' : 'make important'
+  
+    return (
+      <li>
+        {note.content} 
+        <button onClick={toggleImportance}>{label}</button>
+      </li>
+    )
+  }
+  
+  const toggleImportanceOf = (id) => {
+    console.log('importance of ' + id + ' needs to be toggled')
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note}
+          toggleImportance={() => toggleImportanceOf(note.id)}
+           />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>   
+    </div>
+  )
+}
+
+export default App
