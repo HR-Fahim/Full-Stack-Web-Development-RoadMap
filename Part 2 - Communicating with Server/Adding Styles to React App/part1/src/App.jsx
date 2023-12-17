68651a8ab8f9441c260e11775867caef07660801
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-17 23:39:43 +0600

    Add error message and styling for server communication

--- Part 2 - Communicating with Server/Adding Styles to React App/part1/src/App.jsx
+++ Part 2 - Communicating with Server/Adding Styles to React App/part1/src/App.jsx
@@ -24,6 +24,7 @@ const App = () => {
   const [newNote, setNewNote] = useState(
     'a new note...'
   ) 
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService
@@ -65,18 +66,33 @@ const App = () => {
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
-  
+
     noteService
       .update(id, changedNote).then(returnedNote => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
-  
       .catch(() => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
+  }
+
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+  
+    return (
+      <div className='error'>
+        {message}
+      </div>
+    )
   }
 
   const notesToShow = showAll
@@ -91,6 +107,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
