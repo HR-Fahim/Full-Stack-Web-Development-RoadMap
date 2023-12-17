275909b899bf81218da3c00851e8073259a0844e
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-17 23:23:52 +0600

    Add new note and handle server error

--- Part 2 - Communicating with Server/Altering Data in Server/part1/src/App.jsx
+++ Part 2 - Communicating with Server/Altering Data in Server/part1/src/App.jsx
@@ -65,11 +65,17 @@ const App = () => {
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
-
+  
     noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
+      .update(id, changedNote).then(returnedNote => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+  
+      .catch(() => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
       })
   }
 
