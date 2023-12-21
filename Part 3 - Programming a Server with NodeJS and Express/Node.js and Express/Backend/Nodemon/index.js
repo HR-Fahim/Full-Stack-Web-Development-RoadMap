327ecb6c0f55d20011b693e4f633f0e6d6ef25a9
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-22 04:33:51 +0600

    Update API endpoint to delete notes by ID

--- Part 3 - Programming a Server with NodeJS and Express/Node.js and Express/Backend/Nodemon/index.js
+++ Part 3 - Programming a Server with NodeJS and Express/Node.js and Express/Backend/Nodemon/index.js
@@ -23,16 +23,11 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
+  notes = notes.filter(note => note.id !== id)
 
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  response.status(204).end()
 })
 
 const PORT = 3001
