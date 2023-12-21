b6e172dc3d9346f5bbd1e38008b9b91ec8a39e35
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-22 05:18:15 +0600

    Add GET endpoint for retrieving all notes

--- Part 3 - Programming a Server with NodeJS and Express/Node.js and Express/Backend/Nodemon/index.js
+++ Part 3 - Programming a Server with NodeJS and Express/Node.js and Express/Backend/Nodemon/index.js
@@ -31,6 +31,10 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
