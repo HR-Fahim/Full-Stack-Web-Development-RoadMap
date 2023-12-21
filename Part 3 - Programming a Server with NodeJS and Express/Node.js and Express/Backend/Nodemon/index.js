c665c3a746bf472ed74409ecbc70f86113bf11cb
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-12-22 04:47:32 +0600

    Add API endpoint for retrieving a specific note by ID

--- Part 3 - Programming a Server with NodeJS and Express/Node.js and Express/Backend/Nodemon/index.js
+++ Part 3 - Programming a Server with NodeJS and Express/Node.js and Express/Backend/Nodemon/index.js
@@ -19,8 +19,16 @@ let notes = [
   }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
 app.delete('/api/notes/:id', (request, response) => {
