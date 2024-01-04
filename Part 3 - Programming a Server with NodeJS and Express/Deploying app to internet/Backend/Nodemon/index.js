126ba256a4cc646bbf5f89d8613f26f4124cdcc4
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2024-01-04 07:49:08 +0600

    Add API endpoint to retrieve a single note by ID

--- Part 3 - Programming a Server with NodeJS and Express/Deploying app to internet/Backend/Nodemon/index.js
+++ Part 3 - Programming a Server with NodeJS and Express/Deploying app to internet/Backend/Nodemon/index.js
@@ -48,6 +48,12 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
+})
+
 const generateId = () => {
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => n.id))
@@ -58,21 +64,18 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
-  }
+    important: body.important || false,
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
